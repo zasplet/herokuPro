210775e83e2ead9bebe6500c7489f60007718e27
Tadej <zasplet@gmail.com>
2010-10-04 20:20:36 +0200

    nova aplikacija

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+Rails.application.config.secret_token = '68b6291cbb1dec828d431d3139e45ac4c2554bcc3e892ad8d600f1c814eba415812a62085f6603b4063b09fb6a7664fb43bef3d614e351e19b34649e123c41b0'
